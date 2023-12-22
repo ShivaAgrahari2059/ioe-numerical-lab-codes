6693becc2c4d08767c7010c6ca6e8bd7f1589fac
ShivaAgrahari2059 <109414005+ShivaAgrahari2059@users.noreply.github.com>
2023-12-22 13:45:39 +0545

    Update Newton_raphson.cpp

--- Newton_raphson.cpp
+++ Newton_raphson.cpp
@@ -1,21 +1,28 @@
+//this is a simple implementation of Newton-Raphson method using cpp
+
 #include<iostream>
 #include<math.h>
 using namespace std;
 
-float_t f(float_t x)
+//declaration of function
+float_t f(float_t x)   //function can be added here 
 {
-    float_t v=log(x)-cos(x);
-    return v;
+    return(log(x)-cos(x));
+    //return(exp(x)-3*x);
+    //return(x*sin(x)-1);
+    //return(x*log(x)-1.2);
 }
+
+//declaration of first derivative of the function
 float_t f1(float_t y)
 {
     float_t v1=1/y+sin(y);
     return v1;
 }
+
 int main()
 {
     float_t a,b,e;
-    cout<<"f(x)=log(x)-cos(x)"<<endl;
     cout<<"Enter initial point: ";
     cin>>a;
     cout<<"Enter error tolerance: ";
