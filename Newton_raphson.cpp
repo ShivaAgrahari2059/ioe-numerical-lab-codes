8404d2fc2bb209e1b576fd0bbfbe52af25562c29
ShivaAgrahari2059 <109414005+ShivaAgrahari2059@users.noreply.github.com>
2023-12-22 21:45:06 +0545

    Update Newton_raphson.cpp

--- Newton_raphson.cpp
+++ Newton_raphson.cpp
@@ -1,11 +1,11 @@
-//this is a simple implementation of Newton-Raphson method using cpp
+//this is a simple implementation of the Newton-Raphson method using cpp
 
 #include<iostream>
 #include<math.h>
 using namespace std;
 
 //declaration of function
-float_t f(float_t x)   //function can be added here 
+float f(float x)   //functions can be added here
 {
     return(log(x)-cos(x));
     //return(exp(x)-3*x);
@@ -13,34 +13,33 @@ float_t f(float_t x)   //function can be added here
     //return(x*log(x)-1.2);
 }
 
-//declaration of first derivative of the function
-float_t f1(float_t y)
+//declaration of the first derivative of the function
+float f1(float y)
 {
-    float_t v1=1/y+sin(y);
-    return v1;
+    return (1/y+sin(y));
 }
 
 int main()
 {
-    float_t a,b,e;
+    float a,b,e;
     cout<<"Enter initial point: ";
     cin>>a;
     cout<<"Enter error tolerance: ";
     cin>>e;
-    for(int i=0;i<100;i++)
+    for(int i=0;i<100;i++) //process can end just in 5-6 steps/iterations but despite that just in case if we need more
     {
-        if(fabs(f1(a))>0.001)
+        if(fabs(f1(a))>0.001) //f1 should not be very small as it can cause divergence
         {
             b=a-(f(a)/f1(a));
-            if(fabs(f(b))<e)
+            if(fabs(f(b))<e)  //ends the loop as soon as we reach required precision 
             {
                 cout<<"Root= "<<b<<endl;
                 cout<<"No. of iterations= "<<i+1<<endl;
                 return 0;
             }
-            a=b;
+            a=b; //updating value of a
         }
-        else
+        else //if the process takes more than 100 iterations it might be oscillating near the local maxima/minima
         {
             cout<<"cannot proceed due to oscillation";
             return 0;
